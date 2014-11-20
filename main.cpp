19b3148816fdeb28fd34d39824f8574d82909d79
Shivam Garg <jogiya.cse.iitd.ac.in>
2014-11-20 14:06:09 +0530

    with fog

--- main.cpp
+++ main.cpp
@@ -379,6 +379,25 @@ else{
    mt19937 gen(rd());
    uniform_int_distribution<> dis(0, 8);
    
+bool   gp;                      // G Pressed? ( New )
+GLuint filter;                      // Which Filter To Use
+GLuint fogMode[]= { GL_EXP, GL_EXP2, GL_LINEAR };   // Storage For Three Types Of Fog
+GLuint fogfilter= 0;                    // Which Fog To Use
+GLfloat fogColor[4]= {0.5f, 0.5f, 0.5f, 1.0f}; 
+glClearColor(0.5f,0.5f,0.5f,1.0f);          // We'll Clear To The Color Of The Fog ( Modified )
+ 
+glFogi(GL_FOG_MODE, fogMode[fogfilter]);        // Fog Mode
+glFogfv(GL_FOG_COLOR, fogColor);            // Set Fog Color
+glFogf(GL_FOG_DENSITY, 0.010f);              // How Dense Will The Fog Be
+glHint(GL_FOG_HINT, GL_DONT_CARE);          // Fog Hint Value
+glFogf(GL_FOG_START, 1.0f);             // Fog Start Depth
+glFogf(GL_FOG_END, 2.0f);               // Fog End Depth
+glEnable(GL_FOG);     
+
+
+
+
+
 glEnable(GL_LIGHTING);
 glEnable(GL_DEPTH_TEST);
 cout << "size "<< toruss.size() <<  " "<< trees.size() << " "<< stars.size() << endl;
